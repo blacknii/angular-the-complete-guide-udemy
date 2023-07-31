1d54e2486879103ac72e80afe903e92c336990db
blacknii <42908977+blacknii@users.noreply.github.com>
2023-07-31 14:16:14 +0200

    bug fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { FormsModule } from '@angular/forms';
-import { AssignmentTwoComponent } from './assignment-two/assignment-two.component';
-import { AssignmentThreeComponent } from './assignment-three/assignment-three.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent,
-    AssignmentTwoComponent,
-    AssignmentThreeComponent,
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
